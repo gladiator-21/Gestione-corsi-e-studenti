22f6243143f5a51d7ef08d62e861fe8b5063598b
gladiator-21 <imran.8164@isit100.fe.it>
2025-02-15 11:45:03 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'lab.isit100.fe.it';
-$dbname = 'info5n2425_imran';
-$username = 'info5n2425';
-$password = 'Zavaj=87]';
+$host = 'svr';
+$dbname = 'dbname';
+$username = 'uname';
+$password = 'paswd';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
